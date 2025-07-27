d72ecd3ba893ef3a033e983c7a89abe33ffcbd55
Wallace W <wallace_wolv@hotmail.com>
2025-07-27 16:24:54 -0300

    feat: reestrutura layout e navegação do app
    Atualizado o app.component.html para incluir um cabeçalho com links de navegação para "Livros" e "Filmes".
    
    Removido conteúdo de placeholder e integrado o router-outlet para renderização dinâmica de conteúdo.
    
    Adicionado app.component.scss para estilizar o cabeçalho e a área principal.
    
    Simplificado o app.component.ts removendo a propriedade title e os testes relacionados.
    
    Atualizado o app.routes.ts para definir rotas para os componentes de livros e filmes.
    
    Melhorado o index.html com inclusão de meta tags necessárias e link para o Google Fonts.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'books',
+    loadComponent: () =>
+      import('./views/book/book.component').then((m) => m.BookComponent),
+  },
+  {
+    path: 'movies',
+    loadComponent() {
+      return import('./views/movie/movie.component').then(
+        (m) => m.MovieComponent,
+      );
+    },
+  },
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+];
